187c5258616f5dcbe860eadc64e27d791f0aa962
siamand <siamand1381@gmail.com>
2026-06-20 12:44:00 +0330

    feat : connect to supabase

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabase'
+  
+  const scenarios = ref([])
+
+  async function getScenarios() {
+    const { data } = await supabase.from('scenarios').select()
+    scenarios.value = data
+  }
+
+  onMounted(() => {
+    getScenarios()
+  })
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="item in scenarios" :key="item.id">{{ item }}</li>
+  </ul>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
